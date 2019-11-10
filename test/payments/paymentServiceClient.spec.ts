145dab2267412f576ab5115969cbdbe6d45b3d63
David Molinero <davidmolinero.com@gmail.com>
2019-11-10 14:45:44 +0000

    Update tests

--- test/payments/paymentServiceClient.spec.ts
+++ test/payments/paymentServiceClient.spec.ts
@@ -1,41 +1,81 @@
+import pactNode from "@pact-foundation/pact-node";
+
 import Http from "../../src/common/http";
 import { PaymentServiceClient } from "../../src/payments/paymentServiceClient";
 
 // @ts-ignore
-import paymentServicePact from "./paymentServicePact";
+import { default as paymentServicePact, pactBrokerConfig } from "./paymentServicePact";
 
 describe("Payment Service", () => {
-  const pact = paymentServicePact();
+    const pact = paymentServicePact();
 
-  describe("validates a payment method", () => {
-    const validPaymentMethod = "1111111111111111";
-    const status = "valid";
+    describe("validates a payment method", () => {
+        const validPaymentMethod = "1111111111111111";
+        const status = "valid";
 
-    beforeEach(() =>
-        pact
+        beforeEach(() =>
+            pact
             .setup()
             .then(() => pact.addInteraction({
-                  state: "",
-                  uponReceiving: "a request for validating a payment method",
-                  withRequest: {
-                    method: "GET",
-                    path: `/validate-payment-method/${validPaymentMethod}`,
-                    headers: { Accept: "application/json" },
-                  },
-                  willRespondWith: {
-                    status: 200,
-                    headers: { "Content-Type": "application/json" },
-                    body: { status },
-                  }
+                    state: "",
+                    uponReceiving: "a request for validating a payment method",
+                    withRequest: {
+                        method: "GET",
+                        path: `/validate-payment-method/${validPaymentMethod}`,
+                        headers: { Accept: "application/json" },
+                    },
+                    willRespondWith: {
+                        status: 200,
+                        headers: { "Content-Type": "application/json" },
+                        body: { status },
+                    }
                 })
             )
-    );
+        );
 
-    afterEach(async () => await pact.finalize());
+        afterEach(async () => {
+            await pactNode.publishPacts(pactBrokerConfig);
+            await pact.finalize();
+        });
 
-    it("when the payment method is valid", async () => {
-      const response = await new PaymentServiceClient(new Http()).validate(validPaymentMethod);
-      expect(response).toEqual(status);
+        it("when the payment method is valid", async () => {
+            const response = await new PaymentServiceClient(new Http()).validate(validPaymentMethod);
+            expect(response).toEqual(status);
+        });
     });
-  });
+
+    describe("does not validate a payment method", () => {
+        const fraudulentPaymentMethod = "9999999999999999";
+        const status = "fraud";
+
+        beforeEach(() =>
+            pact
+            .setup()
+            .then(() => pact.addInteraction({
+                    state: "fraudulent payment method",
+                    uponReceiving: "a request for validating a payment method",
+                    withRequest: {
+                        method: "GET",
+                        path: `/validate-payment-method/${fraudulentPaymentMethod}`,
+                        headers: { Accept: "application/json" },
+                    },
+                    willRespondWith: {
+                        status: 200,
+                        headers: { "Content-Type": "application/json" },
+                        body: { status },
+                    }
+                })
+            )
+        );
+
+        afterEach(async () => {
+            await pactNode.publishPacts(pactBrokerConfig);
+            await pact.finalize();
+        });
+
+        it("when the payment method is fraudulent", async () => {
+            const response = await new PaymentServiceClient(new Http()).validate(fraudulentPaymentMethod);
+            expect(response).toEqual(status);
+        });
+    });
 });
--- test/payments/paymentServicePact.ts
+++ test/payments/paymentServicePact.ts
@@ -1,18 +1,33 @@
 import * as path from "path";
+import { execSync } from "child_process";
 import { Pact } from "@pact-foundation/pact";
 
 const HOST = "localhost";
-const PORT = 4567;
+const SERVER_PORT = 4567;
+const BROKER_PORT = 8000;
+
+const PACTS_DIR = path.resolve(process.cwd(), "pacts");
 
 const paymentServicePact = () => new Pact({
   host: HOST,
-  port: PORT,
+  port: SERVER_PORT,
   log: path.resolve(process.cwd(), "logs", "mockserver-integration.log"),
-  dir: path.resolve(process.cwd(), "pacts"),
+  dir: PACTS_DIR,
   pactfileWriteMode: "update",
   consumer: "PaymentServiceClient",
   provider: "PaymentService",
   logLevel: "info"
 });
+
+const gitCommit = execSync("git rev-parse HEAD").toString().trim();
+const gitBranch = execSync("git rev-parse --abbrev-ref HEAD").toString().trim();
+
+export const pactBrokerConfig = {
+  pactBroker: process.env.PACT_BROKER_BASE_URL || `http://${HOST}:${BROKER_PORT}`,
+  pactBrokerToken: process.env.PACT_BROKER_TOKEN,
+  pactFilesOrDirs: [PACTS_DIR],
+  consumerVersion: gitCommit,
+  tags: [gitBranch]
+};
 
 export default paymentServicePact;
